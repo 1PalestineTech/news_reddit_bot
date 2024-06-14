a5cb9570d6d0d6eb1da8ee5044642be71915052e
AliTigui <alitighiouart2001@gmail.com>
2024-06-14 21:47:10 +0200

    added some packages and data

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var path = require("path");
-var express = require("express");
-var logger = require("morgan");
-var bodyParser = require("body-parser");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app=express()
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
+var staticPath = path.join(__dirname, "static");
+app.use(express.static(staticPath));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
+app.get("/", function(req, res) {
+    res.render("footer.ejs");
+  })
 
 
 
