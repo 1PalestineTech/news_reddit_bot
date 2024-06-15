045c434bd2ba6664376ee3c0f4e4eb35229b9c30
AliTigui <alitighiouart2001@gmail.com>
2024-06-15 14:12:24 +0200

    working so far

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs=require("fs");
 const app=express()
+
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 var staticPath = path.join(__dirname, "static");
@@ -9,13 +11,59 @@ app.use(express.static(staticPath));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
-    res.render("footer.ejs");
+    res.render("logger.ejs");
+  })
+app.get("/config", function(req, res) {
+  fs.readFile('./config.json', 'utf8', (err, data) => {
+    res.render("config.ejs",{data:data});
+  });
+    
+  })
+  app.post("/config", function(req, res) {
+    
+fs.writeFile('./config.json', req.body.conf, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    res.redirect("config");
+  }
+
+  });
+})
+  app.get("/get_data", function(req, res) {
+    fs.readFile('./config.json', 'utf8', (err, data) => {
+      res.send(data)
+    });
   })
 
 
+
+  app.post("/set_bot", function(req, res) {
+    
+        fs.readFile('./config.json', 'utf8', (err, data) => {
+          if (req.body.bot_data=="start"){
+            fs.writeFile('./config.json', data.replace('"flag":false','"flag":true'), err => {
+              console.log(req.url)
+              res.redirect("/");
+          
+    });
+          }else{
+            fs.writeFile('./config.json', data.replace('"flag":true','"flag":false'), err => {
+
+              res.redirect("/");
+          
+    });
+          }
+
+  })
+})
 
 
 app.listen(3000, function() {
     console.log("App started on port 3000");
   });
-
+async function test(){
+  console.log("test")
+  setTimeout(test,1000);
+}
+test();
---
+++ static/help.js
@@ -0,0 +1,53 @@
+function Start(){
+  
+    fetch("./get_data")
+.then(x =>{
+    console.log(x.text().then(o=>{
+        data=JSON.parse(o);
+        
+        
+        if(data.flag==true){
+        alert("Bot is ON")
+    }else{
+        let flag=document.getElementById("bot_data")
+        flag.value='start'
+        document.getElementById("bot_form").submit();
+
+    }
+
+
+}))
+    
+    
+
+
+})
+    }
+
+    function Stop(){
+        let flag=document.getElementById("bot_data")
+            flag.value='stop'
+        fetch("./get_data")
+    .then(x =>{
+        console.log(x.text().then(o=>{
+            data=JSON.parse(o);
+            
+            
+            if(data.flag==false){
+            alert("Bot is OFF")
+        }else{
+
+            let flag=document.getElementById("bot_data")
+            flag.value='stop'
+            document.getElementById("bot_form").submit();
+    
+        }
+    
+    
+    }))
+        
+        
+    
+    
+    })
+        }
