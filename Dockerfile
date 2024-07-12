55f9be37893e20730c5275868079b90159661b16
AliTigui <alitighiouart2001@gmail.com>
2024-07-12 22:57:29 +0200

    python verion

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM node:slim
+# syntax=docker/dockerfile:1
+FROM python:3.12-slim
 WORKDIR /app
 COPY . /app
-RUN npm install
-EXPOSE 300
-CMD npm start
+RUN pip install -r requirements.txt
+EXPOSE 3000
+
+ENTRYPOINT [ "python" ]
+
+CMD ["app.py"]
---
+++ src/utils.py
@@ -0,0 +1,143 @@
+from time import sleep
+import datetime
+import re
+import sqlite3
+import requests as rq
+import time
+from dateutil import parser
+import bs4 as bs
+import json
+import praw
+import threading
+import os
+db=sqlite3.connect('../data.db')
+def print_L():
+    cursor = db.execute("SELECT * FROM urls")
+    row=cursor.fetchall()
+    print(row)
+
+def write_log(val,file = './logger.txt'):
+    with open(file,'a') as f:
+        f.seek(0)
+        f.write(val + "\n" + str(datetime.datetime.now()) + "\n================================ \n")
+
+def check_regex(regexs,text):
+    if len(regexs) == 0:
+        return True
+    for regex in regexs:
+        if bool(re.search(regex,text,flags = re.IGNORECASE)):
+            return True
+    return False
+def check_url(db,res,regex):
+    title = res['title']
+    if not check_regex(regex,title):
+        res["log"] += "regex didn't match "  +"\n"
+        return (res,False)
+    else:
+        res["log"] += "regex matched \n"
+    cursor = db.execute("SELECT * FROM urls WHERE url = (?) AND sub=(?)",(res["link"],sub))
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        cursor = db.execute('INSERT INTO urls VALUES (?,?)',(res["link"],sub))       
+        res["log"]+="posted : "+res["title"] +"\n"
+        return (res,True)
+    else:
+        res["log"]+="we aleardy posted it \n" 
+        return (res,False)
+
+def get_data(url,time_rang_h,time_rang_m,sub):
+    header = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
+    'referer':'https://www.google.com/'
+    }
+    post = {}
+    now=time.mktime(datetime.datetime.now().timetuple())
+    try:
+        prased = bs.BeautifulSoup(rq.get(url,headers=header).text, "xml")
+    except:
+        return (post,False)
+    data = prased.findAll('entry')
+    if len(data)>1:
+        post["title"] = data[0].findAll('title')[0].text
+        post["link"] = data[0].findAll('link')[0].attrs['href']
+        date = data['feed']['entry'][0]['published']
+
+    else:
+        data = prased.findAll('item')
+        post["title"] = data[0].findAll('title')[0].text
+        post["link"] = data[0].findAll('link')[0].text
+        date = data['rss']['channel']['item'][0]['pubDate']
+    date = time.mktime(parser.parse(date).timetuple())
+    if (now - date) >(time_rang_h*60+time_rang_m)*60:
+        write_log("no new news from :" +url,"./"+sub+".txt")
+        return (post,False)
+    post["log"]="detected new data from :" +url +"\n"+"now testing regex:" +url +"\n"
+    return (post,True)
+
+def url_test(url):
+    header = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
+    'referer':'https://www.google.com/'
+    }
+    post = {}
+    try:
+        prased = bs.BeautifulSoup(rq.get(url,headers=header).text, "xml")
+    except:
+        return  "error link not working"
+    data = prased.findAll('entry')
+    if len(data)>1:
+        post["title"] = data[0].findAll('title')[0].text
+        post["link"] = data[0].findAll('link')[0].attrs['href']
+
+    else:
+        data = prased.findAll('item')
+        post["title"] = data[0].findAll('title')[0].text
+        post["link"] = data[0].findAll('link')[0].text
+    return str(post)
+
+def tread(instance):
+    links = instance['links']
+    SUB_REDDIT = instance['SUB_REDDIT']
+    regex = instance['regex']
+    max_file_size = instance['max_file_size']
+    time_rang_m = instance['time_rang_m']
+    time_rang_h = instance['time_rang_h']
+    post_time_s = instance['post_time_s']
+    post_time_m = instance['post_time_m']
+    userAgent = instance['userAgent']
+    clientId = instance['clientId']
+    clientSecret = instance['clientSecret']
+    refreshToken = instance['refreshToken']
+    if instance['flag']:
+        reddit = praw.Reddit(
+    client_id = clientId,
+    client_secret = clientSecret,
+    refresh_token = refreshToken,
+    user_agent = userAgent,)
+        for link in links:
+            re,f = get_data(link,time_rang_h,time_rang_m,SUB_REDDIT)
+            if f:
+                re,f = check_url(db,re,regex)
+                if f:
+                    reddit.subreddit(SUB_REDDIT).submit(re['title'], url=re['link'])
+                    sleep(post_time_s+post_time_m*60)
+                    write_log(re["log"],"./"+SUB_REDDIT+".txt")
+                    file_stats = os.stat("./"+SUB_REDDIT+".txt")
+                    if file_stats.st_size / (1024 * 1024)>=max_file_size:
+                            with open('./' + SUB_REDDIT +'.txt', 'w') as f:
+                                f.write('')
+
+
+def main():
+    while True:
+        with open('./config.json', 'r') as f:
+            config = json.load(f)
+        threads=[]
+        i=0
+        for instance in config['instances']:
+            threads.append(threading.Thread(target=tread, args=(instance,)))
+        for th in threads:
+            th.start()
+            th.join()
+        sleep(60*5)
+
